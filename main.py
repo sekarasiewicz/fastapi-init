4c2df84fb3e374753eda9079c8e00d378a7fa793
Sebastian Karasiewicz <sekarasiewicz@gmail.com>
2025-06-20 16:44:02 +0200

    Adds model selection endpoint
    Adds an endpoint to select from a predefined set of machine learning models.
    
    The new endpoint accepts a model name and returns a message
    indicating the model selected.

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+from enum import Enum
 from typing import Union
 
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 
 class Item(BaseModel):
@@ -30,6 +37,17 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_id": item_id, "item_name": item.name}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
 
 
 def another_function(a: int, b: str):
