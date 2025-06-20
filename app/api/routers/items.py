d97ddcaa0aa8a39fb3150b04b26785625755a203
Sebastian Karasiewicz <sekarasiewicz@gmail.com>
2025-06-20 21:05:56 +0200

    Renames endpoint for retrieving items
    Renames the `/items/` endpoint from `read_item` to `read_items`
    to better reflect that it retrieves multiple items.

--- app/api/routers/items.py
+++ app/api/routers/items.py
@@ -20,5 +20,5 @@ def update_item(item_id: int, item: Item):
 
 
 @router.get("/items/")
-async def read_item(skip: int = 0, limit: int = 10):
+async def read_items(skip: int = 0, limit: int = 10):
     return fake_items_db[skip : skip + limit]
