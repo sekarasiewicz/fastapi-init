35cc49dbcbc62c04bfd6b8610da29e6cd0e98ccb
Sebastian Karasiewicz <sekarasiewicz@gmail.com>
2025-06-21 14:06:43 +0200

    Enhances item retrieval endpoint with ID validation and random selection
    Updates the `/items/` endpoint to accept an optional `id` parameter, which validates the format to ensure it starts with "isbn-" or "imdb-". If no `id` is provided, a random item is selected from a predefined dataset. The query parameter functionality is also retained, improving the overall item retrieval process.

--- app/api/routers/items.py
+++ app/api/routers/items.py
@@ -1,12 +1,19 @@
+import random
 from typing import Annotated, Union
 
 from fastapi import APIRouter, Query
+from pydantic import AfterValidator
 
 from app.schemas.item import Item
 
 router = APIRouter()
 
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+data = {
+    "isbn-9781529046137": "The Hitchhiker's Guide to the Galaxy",
+    "imdb-tt0371724": "The Hitchhiker's Guide to the Galaxy",
+    "isbn-9781439512982": "Isaac Asimov: The Complete Stories, Vol. 2",
+}
 
 
 @router.get("/items/{item_id}")
@@ -25,14 +32,35 @@ def update_item(item_id: int, item: Item):
 #     return fake_items_db[skip : skip + limit]
 
 
+def check_valid_id(id: str):
+    if not id.startswith(("isbn-", "imdb-")):
+        raise ValueError('Invalid ID format, it must start with "isbn-" or "imdb-"')
+    return id
+
+
 @router.get("/items/")
 async def read_items(
-    q: Annotated[str | None, Query(min_length=3, max_length=50)] = None,
+    q: Annotated[
+        str | None,
+        Query(
+            title="Query string",
+            description="Query string for the items to search in the database that have a good match",
+            min_length=3,
+            max_length=50,
+        ),
+    ] = None,
+    id: Annotated[str | None, AfterValidator(check_valid_id)] = None,
 ):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
-    return results
+        return results
+
+    if id:
+        item = data.get(id)
+    else:
+        id, item = random.choice(list(data.items()))
+    return {"id": id, "name": item}
 
 
 @router.post("/items/")
